b0b5287d7bc13567761e452cec2999c122946466
Anuitex <Anuitex@DESKTOP-00VRJM6>
2019-01-18 18:50:54 +0200

    add gameservice

--- BlackJackCore/Angular/blackjackapp/src/app/app.module.ts
+++ BlackJackCore/Angular/blackjackapp/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GameComponentComponent } from './game-component/game-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GameComponentComponent
   ],
   imports: [
     BrowserModule,
