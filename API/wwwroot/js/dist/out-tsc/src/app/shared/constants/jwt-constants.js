109ace741f9d4899a6f25006f1e564c10d9a224a
Anuitex <Anuitex@DESKTOP-00VRJM6>
2019-04-17 16:29:55 +0300

    test auth guard completed

---
+++ API/wwwroot/js/dist/out-tsc/src/app/shared/constants/jwt-constants.js
@@ -0,0 +1,5 @@
+export var jwtConstant = {
+    userRole: "http://schemas.microsoft.com/ws/2008/06/identity/claims/role",
+    username: "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/name"
+};
+//# sourceMappingURL=jwt-constants.js.map
